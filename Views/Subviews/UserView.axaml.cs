6747dc8aaac664e20854adf3ccaf5debe829bfc9
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2025-05-21 18:10:13 +0545

    Updated
    TODO: FIXES and To add

--- Views/Subviews/UserView.axaml.cs
+++ Views/Subviews/UserView.axaml.cs
@@ -1,5 +1,7 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
 
 namespace MyApp.Views.Subviews
 {
@@ -8,6 +10,27 @@ namespace MyApp.Views.Subviews
         public UserView()
         {
             AvaloniaXamlLoader.Load(this);
+            InitializeComponent();
+
+            AttachedToVisualTree += (_, __) =>
+            {
+                if (this.GetVisualRoot() is Window window)
+                {
+                    void updateMinWidth(object? sender, AvaloniaPropertyChangedEventArgs e)
+                    {
+                        if (e.Property == Window.WidthProperty)
+                        {
+                            CreateUserForm.MinWidth = window.Width - 360;
+                        }
+                    }
+
+                    // Initial set
+                    CreateUserForm.MinWidth = window.Width - 360;
+
+                    // Subscribe to changes
+                    window.PropertyChanged += updateMinWidth;
+                }
+            };
         }
     }
 }
