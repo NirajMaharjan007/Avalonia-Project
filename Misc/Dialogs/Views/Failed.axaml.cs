d67213cf7f059d7e84aab9fb48762ac295004f25
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2025-05-25 17:30:19 +0545

    created dialogs views

---
+++ Misc/Dialogs/Views/Failed.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+
+namespace MyApp.Misc.Dialogs.Views;
+
+public partial class Failed : UserControl
+{
+    public Failed()
+    {
+        InitializeComponent();
+
+        Width = 250;
+        Height = 180;
+    }
+}
---
+++ Misc/Dialogs/Views/Success.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+
+namespace MyApp.Misc.Dialogs.Views;
+
+public partial class Success : UserControl
+{
+    public Success()
+    {
+        InitializeComponent();
+
+        Width = 250;
+        Height = 180;
+    }
+}
